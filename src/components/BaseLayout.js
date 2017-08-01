6b18eeb182fbc9dffe96266b7781f614deb26861
danjsul3 <danny@sullivanconsulting.org>
2017-08-01 16:44:16 -0400

    Styled and built out BaseLayout aka Header and Footer

---
+++ src/components/BaseLayout.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import './BaseLayout.css';
+
+export default class BaseLayout extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return (
+      <div className="base">
+        <nav className="navbar">
+          <img src="../img/logo.svg" />
+        </nav>
+
+        {this.props.children}
+
+        <footer>
+          <div className='footer footerText'>
+            <p>Code by Savannah Cosby, Roxy DeSantis-Nunally, Danny Sullivan</p>
+          </div>
+          <div className='footer roxyGif'>
+            
+          </div>
+          <div className='footer savGif'>
+            
+          </div>
+          <div className='footer dannyGif'>
+            
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-
 class Results extends Component {
     constructor(props) {
         super(props)
